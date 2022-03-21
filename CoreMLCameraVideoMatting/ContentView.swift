1d93e5fc2a97120953ee8d0b8add591ac5cf8914
Dmitry Voitekh <d.voitekh@gmail.com>
2022-03-21 10:44:42 +0200

    Initial Commit

---
+++ CoreMLCameraVideoMatting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreMLCameraVideoMatting
+//
+//  Created by Dmitry Voitekh on 21.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreMLCameraVideoMatting/CoreMLCameraVideoMattingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLCameraVideoMattingApp.swift
+//  CoreMLCameraVideoMatting
+//
+//  Created by Dmitry Voitekh on 21.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLCameraVideoMattingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
